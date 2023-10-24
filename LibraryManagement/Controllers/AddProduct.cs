333c80f26a18cb05f9e8bda88fbd956d00a95a3d
infy_1177288 <infy_1177288@outlook.com>
2023-10-24 18:17:58 +0530

    Add project files.

---
+++ LibraryManagement/Controllers/AddProduct.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class AddProduct : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
